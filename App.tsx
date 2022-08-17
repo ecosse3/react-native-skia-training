d73fe816aff843c4b06917f78f8e5f1d2dc4138c
ecosse3 <luk.kurpiewski@gmail.com>
2022-08-17 14:48:17 +0200

    feat: create stack navigator with Examples initial route

--- App.tsx
+++ App.tsx
@@ -1,20 +1,22 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import type { Routes } from './src/Routes';
+import { Examples } from './src/screens/Examples';
+
+const Stack = createNativeStackNavigator<Routes>();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <NavigationContainer>
+        <StatusBar />
+        <Stack.Navigator initialRouteName="Examples">
+          <Stack.Screen name="Examples" component={Examples} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </GestureHandlerRootView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
